249caaecd3d91c6f0c58840f0e7eb6dcc8a17a7c
zhang liangwang <zhangliangwang@Mac-mini.local>
2017-03-28 16:26:02 +0800

    基于NSURLSessionDataTask\NSOutputStream实现离线断点下载的

---
+++ TTBreakPointDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTBreakPointDemo
+//
+//  Created by zhang liangwang on 17/3/27.
+//  Copyright © 2017年 zhangliangwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
